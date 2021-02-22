81120957d73678a06d11fedcf475ef5ce0e5c732
cizamihigo <cizamihigochristian@gmail.com>
2021-02-23 01:02:06 +0200

    delta using     termux for phones

---
+++ delta.py
@@ -0,0 +1,10 @@
+#git 
+
+print("welcome to my program")
+a = int(input("*Enter a number* :  "))
+b = int(input("*Enter a number* :  "))
+c = int(input("*Enter a number* :  "))
+
+delta = b* b -(4 *a * c)
+
+print(delta)
