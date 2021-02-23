36626b3c2d5da8e82d333e62f395bc1cd8f25f03
cizamihigo <cizamihigochristian@gmail.com>
2021-02-23 20:05:18 +0200

    x values implemented from phone pydroid.

--- delta.py
+++ delta.py
@@ -1,4 +1,5 @@
 #git 
+import math
 
 print("welcome to my program")
 a = int(input("*Enter a number* :  "))
@@ -7,4 +8,21 @@ c = int(input("*Enter a number* :  "))
 
 delta = b* b -(4 *a * c)
 
-print(delta)
+if delta == 0 : 
+    vardelta =0
+    vardelta = (-b)/ 2*a
+    #print(vardelta)
+elif delta > 0 :
+    vardelta1 = 0.1
+    vardelta2 = 0.1
+    
+    vardelta1 =  (-b + (delta * 0.5)) / (2* a)
+    
+    vardelta2 =  (-b - (delta * 0.5)) / (2* a)
+    
+    print("X1 =   ", vardelta1)
+    #print(vardelta2)
+    
+    
+    print("X2 =   ", vardelta2)
+    
